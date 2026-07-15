72d123acb2f0605190e7369ab6c8514a7a4e6f81
remohammed43-code <remohammed43@gmail.com>
2026-07-15 15:45:15 +0300

    Frist Commit

---
+++ main.js
@@ -0,0 +1,89 @@
+
+let Num_1 ;
+let Num_2 ;
+let Operation;
+let result;
+function Plus(Num_1,Num_2) {
+return Num_1+Num_2    
+} 
+function Minus(Num_1,Num_2) {
+return Num_1-Num_2    
+} 
+function Multi(Num_1,Num_2) {
+return Num_1*Num_2    
+} 
+function Dev(Num_1,Num_2) {
+return Num_1/Num_2    
+} 
+const display = document.getElementById("display")
+const buttons =document.querySelectorAll("button")
+buttons.forEach(function(button) {
+    button.addEventListener("click",function() {
+      
+          if (button.textContent === "C") {
+            display.textContent = "0"
+            
+        }
+        else if(button.textContent ==="+"){
+              Num_1 =Number(display.textContent)
+              Operation ="+"
+              display.textContent =0;
+             
+             
+              
+        }
+        else if(button.textContent ==="-"){
+              Num_1 =Number(display.textContent)
+              Operation ="-"
+              display.textContent =0;
+             
+             
+              
+        }
+        else if(button.textContent ==="*"){
+              Num_1 =Number(display.textContent)
+              Operation ="*"
+              display.textContent =0;
+             
+             
+              
+        }
+        else if(button.textContent ==="/"){
+              Num_1 =Number(display.textContent)
+              Operation ="/"
+              display.textContent =0;
+             
+             
+              
+        }
+        else if(button.textContent ==="="){
+            Num_2 =Number(display.textContent) 
+             if (Operation === "+") {
+                result = Plus(Num_1,Num_2)
+                display.textContent =result
+             }
+             if (Operation === "-") {
+                result = Minus(Num_1,Num_2)
+                display.textContent =result
+             }
+             if (Operation === "*") {
+                result = Multi(Num_1,Num_2)
+                display.textContent =result
+             }
+             if (Operation === "/") {
+                result = Dev(Num_1,Num_2)
+                display.textContent =result
+             }
+
+        }
+        
+        
+        else{
+            display.textContent =display.textContent + button.textContent
+        }
+
+      
+    }
+)
+})
+
